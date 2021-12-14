d1687565d8e0014a6562409050f2de5cace7aa1b
Lin yi ping <95246311+yiping560@users.noreply.github.com>
2021-12-14 16:48:40 +0800

    1101214

--- 0705_Game_Animation/src/main/java/tw/tcnr07/m0705/M0705.java
+++ 0705_Game_Animation/src/main/java/tw/tcnr07/m0705/M0705.java
@@ -4,7 +4,9 @@ import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
@@ -17,20 +19,23 @@ import android.widget.ViewSwitcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class M0705 extends AppCompatActivity implements ViewSwitcher.ViewFactory, AdapterView.OnItemClickListener {
+public class M0705 extends AppCompatActivity implements
+        ViewSwitcher.ViewFactory,
+        AdapterView.OnItemClickListener {
 
     private TextView s001;
     private TextView f000;
     private String user_select;
     private String answer;
     private ImageButton b001, b002, b003;
-    private ImageView c001;
+    private ImageSwitcher c001;
     private MediaPlayer startmusic;
     private MediaPlayer mediaWin;
     private MediaPlayer mediaLose;
     private MediaPlayer mediaDraw;
     private MediaPlayer goodnightmusic;
     private RelativeLayout r_layout;
+    private ImageSwitcher imgSwi_comp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +48,14 @@ public class M0705 extends AppCompatActivity implements ViewSwitcher.ViewFactory
         b001 = (ImageButton) findViewById(R.id.m0705_b001);
         b002 = (ImageButton) findViewById(R.id.m0705_b002);
         b003 = (ImageButton) findViewById(R.id.m0705_b003);
-        c001 = (ImageView) findViewById(R.id.m0705_c001);
+        c001 = (ImageSwitcher) findViewById(R.id.m0705_c001);
+        //---電腦出拳---
+        imgSwi_comp = (ImageSwitcher) findViewById(R.id.m0705_c001);
+        imgSwi_comp.setFactory(this);
+
+                //--設定imageutton初始值為全透明B
+                        u_setalpha();
+
         s001 = (TextView) findViewById(R.id.m0705_s001);
         f000 = (TextView) findViewById(R.id.m0705_f000);
         //--設定imageButton初始值為全透明
@@ -63,17 +75,21 @@ public class M0705 extends AppCompatActivity implements ViewSwitcher.ViewFactory
         mediaLose = MediaPlayer.create(getApplication(), R.raw.lose);
         mediaDraw = MediaPlayer.create(getApplication(), R.raw.haha);
 
+
         b001.setOnClickListener(b001on);
         b002.setOnClickListener(b001on);
         b003.setOnClickListener(b001on);
     }
 
     private void u_setalpha() {  //imageButton 背景為銀色且為全透明
-        b001.setBackgroundColor(Color.GRAY);
+        //b001.setBackgroundColor(Color.GRAY);
+        b001.setBackgroundResource(R.drawable.circle_shape);
         b001.getBackground().setAlpha(0);  //0-255
-        b002.setBackgroundColor(Color.GRAY);
+//        b002.setBackgroundColor(Color.GRAY);
+        b002.setBackgroundResource(R.drawable.circle_shape);
         b002.getBackground().setAlpha(0);  //0-255
-        b003.setBackgroundColor(Color.GRAY);
+//        b003.setBackgroundColor(Color.GRAY);
+        b003.setBackgroundResource(R.drawable.circle_shape);
         b003.getBackground().setAlpha(0);  //0-255
         //imageButton 背景為銀色且為全透明，此為舊寫法，之後要用如上寫法
 //        btnScissors.setBackgroundColor(ContextCompat.getColor(this, R.color.Silver));
@@ -84,10 +100,11 @@ public class M0705 extends AppCompatActivity implements ViewSwitcher.ViewFactory
 //        btnNet.getBackground().setAlpha(0);
     }
 
+
     private View.OnClickListener b001on = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            int iComPlay = (int) (Math.random() * 3 + 1); //亂數1剪刀2石頭3布
+                        int iComPlay = (int) (Math.random() * 3 + 1); //亂數1剪刀2石頭3布
             // 1 - scissors, 2 - stone, 3 - net.
             u_setalpha();
             switch (v.getId()) {
@@ -166,68 +183,74 @@ public class M0705 extends AppCompatActivity implements ViewSwitcher.ViewFactory
                     user_select = getString(R.string.m0705_s001) + " " + getString(R.string.m0705_b003);
                     break;
             }
+            //--------電腦出拳增加動畫---------------
+            imgSwi_comp.clearAnimation();
+            Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_trans_bounce); //down
+            anim.setInterpolator(new BounceInterpolator()); //jump
+            imgSwi_comp.setAnimation(anim);
+            //---------------------------------------
             s001.setText(user_select);
             f000.setText(answer);
+
         }
-
-
     };
 
-    private void music(int i) {
-        if (startmusic.isPlaying()) startmusic.stop();
-        if (mediaWin.isPlaying()) mediaWin.pause();
-        if (mediaDraw.isPlaying()) mediaDraw.pause();
-        if (mediaLose.isPlaying()) mediaLose.pause();
-        switch (i) {
-            case 1:  //贏
-                mediaWin.start();
-                Toast.makeText(getApplicationContext(), R.string.m0705_f001, Toast.LENGTH_LONG).show();
-                break;
-            case 2:  //平
-                mediaDraw.start();
-                Toast.makeText(getApplicationContext(), R.string.m0705_f003, Toast.LENGTH_LONG).show();
-                break;
-            case 3:  //輸
-                mediaLose.start();
-                Toast.makeText(getApplicationContext(), R.string.m0705_f002, Toast.LENGTH_LONG).show();
-                break;
-            case 4:  //結束程式
-                goodnightmusic.start();
-                break;
+
+        private void music(int i) {
+            if (startmusic.isPlaying()) startmusic.stop();
+            if (mediaWin.isPlaying()) mediaWin.pause();
+            if (mediaDraw.isPlaying()) mediaDraw.pause();
+            if (mediaLose.isPlaying()) mediaLose.pause();
+            switch (i) {
+                case 1:  //贏
+                    mediaWin.start();
+                    Toast.makeText(getApplicationContext(), R.string.m0705_f001, Toast.LENGTH_LONG).show();
+                    break;
+                case 2:  //平
+                    mediaDraw.start();
+                    Toast.makeText(getApplicationContext(), R.string.m0705_f003, Toast.LENGTH_LONG).show();
+                    break;
+                case 3:  //輸
+                    mediaLose.start();
+                    Toast.makeText(getApplicationContext(), R.string.m0705_f002, Toast.LENGTH_LONG).show();
+                    break;
+                case 4:  //結束程式
+                    goodnightmusic.start();
+                    break;
+            }
+        }
+
+        @Override
+        public void onBackPressed() {
+            super.onBackPressed();
+            music(4);
+            this.finish();
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            if (startmusic.isPlaying()) startmusic.stop();
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+
+        @Override
+        public View makeView() {
+            ImageView v = new ImageView(this);
+            // v.setBackgroundColor(0xFF000000);
+            v.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            v.setLayoutParams(new
+                    ImageSwitcher.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            return v;
+        }
+
+        @Override
+        public void onPointerCaptureChanged(boolean hasCapture) {
+
         }
     }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        music(4);
-        this.finish();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (startmusic.isPlaying()) startmusic.stop();
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-    @Override
-    public View makeView() {
-        ImageView v = new ImageView(this);
-        // v.setBackgroundColor(0xFF000000);
-        v.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        v.setLayoutParams(new
-                ImageSwitcher.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        return v;
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-}
